f95e8fd240357ac21d4d14af1856941116bd0620
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2020-05-19 16:56:22 +0300

    UPLOAD

---
+++ DB/SQLQuery2.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Users(
+	Id BIGINT PRIMARY KEY IDENTITY NOT NULL,
+	Username VARCHAR(30) UNIQUE NOT NULL,
+	[Password] VARCHAR(26) NOT NULL,
+	ProfilePicture VARBINARY(MAX) CHECK (DATALENGTH(ProfilePicture) <= 900 * 1024),
+	LastLoginTime DATETIME2 NOT NULL,
+	IsDelited BIT NOT NULL
+)
+INSERT INTO Users(Username,[Password],LastLoginTime,IsDelited)VALUES('Pesho0','123456','05.19.2020',0),('Pesho1','123456','05.19.2020',1),('Pesho2','123456','05.19.2020',0),('Pesho3','123456','05.19.2020',0),('Pesho4','123456','05.19.2020',1)
+
+SELECT * FROM Users
+
+ALTER TABLE Users
+DROP CONSTRAINT [PK__Users__3214EC07DC0270AE]
+
+ALTER TABLE Users ADD CONSTRAINT PK_Users_CompositeIdUsername
+PRIMARY KEY (Id,Username)
+
+ALTER TABLE Users ADD CONSTRAINT CH_User_PasswordLength CHECK (LEN([Password]) >= 5)
+
+ALTER TABLE Users ADD CONSTRAINT DF_Users_LastLoginTime DEFAULT GETDATE() FOR LastLoginTime
+
+ALTER TABLE Users DROP CONSTRAINT PK_Users_CompositeIdUsername
+
+ALTER TABLE Users ADD CONSTRAINT CH_Users_
